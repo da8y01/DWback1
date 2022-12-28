018e997a4e1f92d0a3ff2431b3af31a8a574c9db
Developer <developer@local.host>
2022-12-27 19:04:07 -0500

    Initial configuration for SQL Server, connection string.

--- GenericApi1/Models/TodoContext.cs
+++ GenericApi1/Models/TodoContext.cs
@@ -5,6 +5,18 @@ namespace GenericApi1.Models
 {
     public class TodoContext : DbContext
     {
+        private string _connectionString = "Server=tcp:transact1.database.windows.net,1433;Database=decowraps1;User ID=admin1;Password=Transact1.;Trusted_Connection=False;Encrypt=True;";
+
+        public TodoContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        
         public TodoContext(DbContextOptions<TodoContext> options)
             : base(options)
         {
--- GenericApi1/Program.cs
+++ GenericApi1/Program.cs
@@ -1,14 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using GenericApi1.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
+
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+    //opt.UseInMemoryDatabase("TodoList"));
+    opt.UseSqlServer(config.GetConnectionString("DW1Database")));
+
 //builder.Services.AddSwaggerGen(c =>
 //{
 //    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
