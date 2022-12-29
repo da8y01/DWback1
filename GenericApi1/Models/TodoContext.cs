5b05003c31a6fb10bc08107d73db8edfe302537b
Developer <developer@local.host>
2022-12-28 21:34:46 -0500

    Change connection string for the one from portal.azure.com

--- GenericApi1/Models/TodoContext.cs
+++ GenericApi1/Models/TodoContext.cs
@@ -5,7 +5,7 @@ namespace GenericApi1.Models
 {
     public class TodoContext : DbContext
     {
-        private string _connectionString = "Server=tcp:transact1.database.windows.net,1433;Database=decowraps1;User ID=admin1;Password=Transact1.;Trusted_Connection=False;Encrypt=True;";
+        private string _connectionString = "Server=tcp:transact1.database.windows.net,1433;Initial Catalog=decowraps1;Persist Security Info=False;User ID=admin1;Password=Transact1.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public TodoContext(string connectionString)
         {
