c99faa03315ea5f4c3b845a05272519cf1f08afb
Developer <developer@local.host>
2022-12-29 01:44:13 -0500

    Change TodoItem model IsComplete field data type from bool to int due to Azure reasons.

--- GenericApi1/Models/TodoItemDTO.cs
+++ GenericApi1/Models/TodoItemDTO.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public string? Name { get; set; }
-        public bool IsComplete { get; set; }
+        //public bool IsComplete { get; set; }
+        public int IsComplete { get; set; }
     }
 }
